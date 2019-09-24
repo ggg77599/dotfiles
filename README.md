5dd1d00471dde2c610ad67478bdf35af67c26cbc
MrG <ggg77599@gmail.com>
2019-09-24 14:42:10 +0000

    add help in install.sh

--- README.md
+++ README.md
@@ -1,44 +1,25 @@
 # Dot Files
 
-Include bashrc, vimrc, sqliterc, tmux.conf ...
-
-## Description
-three mode of install the dotfiles,
-
-## Install
-
-* minimal mode (bashrc + vimrc)
-```bash
-sudo sh install.sh
-```
-
-* basic mode (linux packages + vimrc)
-```bash
-sudo sh install.sh basic
-```
-
-* developer mode (linux packages + vim package)
-```bash
-sudo sh install.sh dev
-```
+Include bashrc, vimrc, gitconf, sqliterc, tmux.conf ...
 
 ## Usage
-* install ( minimal, basic, developer )
-* update
-    * update config
-* uninstall
-    * remove config
-* reinstall
-    * uninstall + install
+
+```bash
+sh install.sh min              # bashrc + vimrc
+sh install.sh basic            # linux packages + vimrc
+sh install.sh dev              # linux packages + vim package
+sh install.sh devycm           # linux packages + vim package + YCM
+```
+
+## To Do List
+- [ ] reference DockerTest DockerFile
+    - [ ] test in container
+- [ ] rearrange bashrc
+- [ ] rearrange vimrc
 
 ## bashrc
-
+* difference level of PS1
 
 ## vimrc
 * https://vimconfig.com
-
-## To Do List
-* reference DockerTest DockerFile
-* set time zone
-* set locales
 
--- install.sh
+++ install.sh
@@ -6,8 +6,14 @@
 # Distributed under terms of the MIT license.
 #
 
-## setup command prefix
-dotfiles_HOME=`pwd`
+printhelp()
+{
+    echo "Usage of install.sh"
+    echo "    sh install.sh min              # bashrc + vimrc"
+    echo "    sh install.sh basic            # linux packages + vimrc"
+    echo "    sh install.sh dev              # linux packages + vim package"
+    echo "    sh install.sh devycm           # linux packages + vim package + YCM"
+}
 
 updateVim()
 {
@@ -25,130 +31,137 @@ updateVim()
     fi
 }
 
-echo "============================================================"
-echo "#             install minimal (bashrc + vimrc)             #"
-echo "============================================================"
-
-echo "=================================================== install packages"
-sudo apt-get update -y
-sudo apt-get install -y colordiff \
-                           git \
-                           vim \
-                           python3-minimal \
-                           ssh \
-                           curl
-
-echo "========================================================== setup git"
-# install git lfs
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install -y git-lfs
-git lfs install
-
-# git completion and prompt
-version=`git --version | awk '{print $3}'`
-wget "https://raw.githubusercontent.com/git/git/v"$version"/contrib/completion/git-completion.bash" -O ~/.git-completion.bash
-wget "https://raw.githubusercontent.com/git/git/v"$version"/contrib/completion/git-prompt.sh"       -O ~/.git-prompt.sh
-
-# setup git
-cp gitconfig ~/.gitconfig
-
-echo "======================================================= setup bashrc"
-if grep -q ". ~/.bashrc.mrg" ~/.bashrc; then
-    echo update myRC
-else
-    echo ". ~/.bashrc.mrg" >> ~/.bashrc
-    echo installing myRC
-fi
-cp bashrc ~/.bashrc.mrg
-
-echo "========================================================== setup vim"
-cp vimrc.mini ~/.vimrc
-
 
 if [ -z $1 ] ; then  # if no $1, install minimal only
+    printhelp
     exit
-    echo 
 fi
 
-if [ $1 = "basic" ] || [ $1 = "dev" ] || [ $1 = "devycm" ] ; then
+if [ $1 = "min" ] || [ $1 = "basic" ] || [ $1 = "dev" ] || [ $1 = "devycm" ] ; then
 
+    ## setup command prefix
+    dotfiles_HOME=`pwd`
     echo "============================================================"
-    echo "#          install minimal (linux packages + vimrc)        #"
+    echo "#             install minimal (bashrc + vimrc)             #"
     echo "============================================================"
 
+    echo "============================================================ install packages"
+    sudo apt-get update -y
+    sudo apt-get install -y colordiff \
+                            git \
+                            vim \
+                            python3-minimal \
+                            ssh \
+                            curl
 
-    echo "=================================================== install packages"
-    sudo apt-get install -y build-essential \
-                               python3-dev \
-                               python3-distutils \
-                               tmux \
-                               nfs-common \
-                               htop \
-                               iputils-ping \
-                               net-tools \
-                               locales
-
-    echo "======================================================= setup python"
-    # install pip for python 3
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    sudo -H python3 get-pip.py
-    rm -f get-pip.py
-
+    echo "============================================================ setup git"
     # python symbolic link
     cd /usr/bin/
     sudo ln -s -f python3 python
     cd $dotfiles_HOME
 
-    ## setup pip
-    mkdir -p ~/.pip/
-    cp pip.conf ~/.pip/
+    # install git lfs
+    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+    sudo apt-get install -y git-lfs
+    git lfs install
 
-    # install pip packages
-    sudo -H pip3 install virtualenv
+    # git completion and prompt
+    version=`git --version | awk '{print $3}'`
+    wget "https://raw.githubusercontent.com/git/git/v"$version"/contrib/completion/git-completion.bash" -O ~/.git-completion.bash
+    wget "https://raw.githubusercontent.com/git/git/v"$version"/contrib/completion/git-prompt.sh"       -O ~/.git-prompt.sh
 
-    echo "========================================================= fix locale"
-    export LC_ALL="en_US.UTF-8"
-    sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+    # setup git
+    cp gitconfig ~/.gitconfig
 
-
-    echo "========================================================= setup tmux"
-    # setup tmux config
-    cp tmux.conf ~/.tmux.conf
-
-    if [ $1 = "dev" ] ; then
-        echo "============================================================"
-        echo "#         install dev (linux packages + vim package)       #"
-        echo "============================================================"
-
-        echo "========================================================== setup vim"
-        cp vimrc ~/.vimrc
-
-        echo "=========================================== install vim packages"
-        updateVim
-
-    elif [ $1 = "devycm" ] ; then
-        echo "============================================================"
-        echo "#   install devycm (linux packages + vim package + YCM)    #"
-        echo "============================================================"
-
-        echo "=================================================== install packages"
-        sudo apt-get install -y exuberant-ctags \
-                                   cmake
-
-        echo "========================================================== setup vim"
-        cp vimrc ~/.vimrc
-        sed -i -e 's/\"Plugin/Plugin/g' ~/.vimrc
-
-        # copy C-family Semantic Completion Engine
-        wget https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
-
-        echo "=========================================== install vim packages"
-        updateVim
-
-        cd ~/.vim/bundle/YouCompleteMe
-        ./install.py
+    echo "============================================================ setup bashrc"
+    if grep -q ". ~/.bashrc.mrg" ~/.bashrc; then
+        echo update myRC
+    else
+        echo ". ~/.bashrc.mrg" >> ~/.bashrc
+        echo installing myRC
     fi
-fi
+    cp bashrc ~/.bashrc.mrg
 
+    echo "============================================================ setup vim"
+    cp vimrc.mini ~/.vimrc
+
+
+    if [ $1 = "basic" ] || [ $1 = "dev" ] || [ $1 = "devycm" ] ; then
+
+        echo "============================================================"
+        echo "#          install minimal (linux packages + vimrc)        #"
+        echo "============================================================"
+
+
+        echo "============================================================ install packages"
+        sudo apt-get install -y build-essential \
+                                python3-dev \
+                                python3-distutils \
+                                tmux \
+                                nfs-common \
+                                htop \
+                                iputils-ping \
+                                net-tools \
+                                locales
+
+        echo "============================================================ setup python"
+        # install pip for python 3
+        curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+        sudo -H python3 get-pip.py
+        rm -f get-pip.py
+
+        ## setup pip
+        mkdir -p ~/.pip/
+        cp pip.conf ~/.pip/
+
+        # install pip packages
+        sudo -H pip3 install virtualenv
+
+        echo "============================================================ fix locale"
+        export LC_ALL="en_US.UTF-8"
+        sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+
+
+        echo "============================================================ setup tmux"
+        # setup tmux config
+        cp tmux.conf ~/.tmux.conf
+
+        if [ $1 = "dev" ] ; then
+            echo "============================================================"
+            echo "#         install dev (linux packages + vim package)       #"
+            echo "============================================================"
+
+            echo "============================================================ setup vim"
+            cp vimrc ~/.vimrc
+
+            echo "============================================================ install vim packages"
+            updateVim
+
+        elif [ $1 = "devycm" ] ; then
+            echo "============================================================"
+            echo "#   install devycm (linux packages + vim package + YCM)    #"
+            echo "============================================================"
+
+            echo "============================================================ install packages"
+            sudo apt-get install -y exuberant-ctags \
+                                    cmake
+
+            echo "============================================================ setup vim"
+            cp vimrc ~/.vimrc
+            sed -i -e 's/\"Plugin/Plugin/g' ~/.vimrc
+
+            # copy C-family Semantic Completion Engine
+            wget https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+
+            echo "============================================================ install vim packages"
+            updateVim
+
+            cd ~/.vim/bundle/YouCompleteMe
+            ./install.py
+        fi
+    fi
+else
+    printhelp
+fi
 
 
