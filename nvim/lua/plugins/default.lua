59bada6747cb9508343028c5838331cc6d862cfa
MrG <ggg77599@gmail.com>
2024-09-22 22:51:18 +0800

    backup

--- nvim/lua/plugins/default.lua
+++ nvim/lua/plugins/default.lua
@@ -1,6 +1,15 @@
 return {
-	"preservim/tagbar",
-	config = function()
-		vim.keymap.set("n", "<F8>", vim.cmd.TagbarToggle)
-	end,
+  {
+    "preservim/tagbar",
+    config = function()
+      vim.keymap.set("n", "<F8>", vim.cmd.TagbarToggle)
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require('todo-comments').setup()
+    end
+  }
 }
