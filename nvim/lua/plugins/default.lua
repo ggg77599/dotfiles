bf5a7f1a3abc4a6c9c7675a1904720e4b77acc9a
ggg77599 <ggg77599@gmail.com>
2024-09-26 02:00:35 +0800

    backup and add git-wt

--- nvim/lua/plugins/default.lua
+++ nvim/lua/plugins/default.lua
@@ -1,15 +1,25 @@
 return {
-  {
-    "preservim/tagbar",
-    config = function()
-      vim.keymap.set("n", "<F8>", vim.cmd.TagbarToggle)
-    end,
-  },
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require('todo-comments').setup()
-    end
-  }
+	{
+		"preservim/tagbar",
+		config = function()
+			vim.keymap.set("n", "<F8>", vim.cmd.TagbarToggle)
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{
+		"tpope/vim-surround",
+	},
+	{
+		-- for vim-surround able to use . to repeat
+		"tpope/vim-repeat",
+	},
+	{
+		"vim-airline/vim-airline",
+	},
 }
