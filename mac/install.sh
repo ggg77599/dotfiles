644ae44f282e9fa4cd84ee38374d57cefd857549
ggg77599 <ggg77599@gmail.com>
2025-05-23 23:32:10 +0800

    update install package

--- mac/install.sh
+++ mac/install.sh
@@ -8,12 +8,19 @@ brew install bash-completion # add source command before setting PS1
 brew install tree
 brew install fzf     # fuzzy search
 brew install ripgrep # grep alternative
+brew install fd      # find alternative
 brew install wget
 brew install tldr # man alternative
 brew install sops
-brew install vim
 brew install neovim
-#brew deps --tree --installed
+brew install docker
+brew install docker-buildx
+brew install docker-compose
+brew install docker-credential-helper
+brew install docker-completion
+brew install colima # docker alternative ( open source )
+# list all dependancy
+#   brew deps --tree --installed
 
 # install software
 # https://formulae.brew.sh/cask/
@@ -23,13 +30,13 @@ brew install --cask google-chrome
 brew install --cask iina
 brew install --cask microsoft-remote-desktop
 brew install --cask visual-studio-code
-brew install --cask notion
+brew install --cask notion # note taking
 brew install --cask rectangle
 brew install --cask sublime-text
-brew install --cask tunnelblick
-brew install --cask typora
-brew install --cask logseq
-brew install --cask docker
+brew install --cask tunnelblick # openvpn client
+brew install --cask typora      # markdown editor
+brew install --cask obsidian    # note taking
+brew install --cask orbstack
 brew install --cask toggl-track
 
 # at least open those app once
@@ -37,7 +44,11 @@ brew install --cask --no-quarantine qlmarkdown       # preview markdown
 brew install --cask --no-quarantine syntax-highlight # preview code with highlight
 
 # language support
-brew install fnm    # node.js version manager
-brew install rustup # rust version manager
+brew install fnm    # node.js version manager, for neovim mason to install packages
+brew install rustup # rust version manager, use rustup-init to install
 brew install go
 brew install python
+
+## install go packages
+go install github.com/rakyll/gotest  # colorful test results
+go install github.com/cweill/gotests # generate test file
